30a15cfef7e42ad87cec0e0f474cc170abd90005
Flavio <flaviostincone711@gmail.com>
2026-01-19 10:30:01 +0100

    completamento dei test
    aggiunto l'inserimento di Confidential/Secret/Public Integration Test

--- documenti/src/test/java/exprivia/it/Documenti/controller/PublicDocumentControllerTest.java
+++ documenti/src/test/java/exprivia/it/Documenti/controller/PublicDocumentControllerTest.java
@@ -13,8 +13,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,7 +31,7 @@ class PublicDocumenControllerTest {
     @Autowired
     private ObjectMapper objectMapper; 
 
-    @MockitoBean 
+    @MockBean 
     private IPublicDocument service; 
 
     @Nested
